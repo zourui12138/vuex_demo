bb2cac2de289a6159d54f7203eaa29f3da1acadb
zourui <zourui@mrray.cn>
2018-05-09 09:25:16 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -22,8 +22,8 @@
         mounted() {
             console.log(this);
             // 你不能直接调用一个 mutation handler。
-            // 这个选项更像是事件注册：“当触发一个类型为 increment 的 mutation 时，调用此函数。
-            // ”要唤醒一个 mutation handler，你需要以相应的 type 调用 store.commit 方法
+            // 这个选项更像是事件注册：“当触发一个类型为 increment 的 mutation 时，调用此函数”。
+            // 要唤醒一个 mutation handler，你需要以相应的 type 调用 store.commit 方法
             // 你可以向 store.commit 传入额外的参数，即 mutation 的 载荷（payload）
             this.increment(10);
             // 在大多数情况下，载荷应该是一个对象，这样可以包含多个字段并且记录的 mutation 会更易读
